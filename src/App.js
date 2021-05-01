7baf6388f8daad5167834dfcf6e146c371a0ad85
Shane McGinley <shane.mcginley@students.ittralee.ie>
2021-05-01 13:16:28 +0100

    Adding Nav Bar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  return ( 
+    <div className="App" id="myTopnav">
+      <a href="#home" class="active">Home</a>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
