20ad7d5a60f4968f4c26f18c67b2a5730f573bc7
Shane McGinley <shane.mcginley@students.ittralee.ie>
2021-05-01 13:30:26 +0100

    Fixing Nav Bar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 
 function App() {
   return ( 
-    <div className="App" id="myTopnav">
-      <a href="#home" class="active">Home</a>
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
-      </a>
+    function NavBar() {
+      return ( 
+        <div className="topnav" id="myTopnav">
+          <a href="#home" class="active">Home</a>
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+          </a>
+        </div>
+      );
+    },
+
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
